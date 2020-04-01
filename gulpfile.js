375691c9584e5ce5fcef70667d8374629736184d
Marcial Garcia <garcia.dfe@gmail.com>
2020-04-01 16:45:24 -0300

    Adjust config gulp for test Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
-const cssmin = require("gulp-clean-css");
-const concat = require("gulp-concat");
-const rename = require("gulp-rename");
-const uglify = require("gulp-uglifyes");
-const imagemin = require("gulp-imagemin");
-const htmlreplace = require("gulp-html-replace");
-const htmlmin = require("gulp-htmlmin");
 
 // Compilar SASS gerando CSS na pasta SRC
 function compilaSASS() {
@@ -20,61 +13,14 @@ function compilaSASS() {
     .pipe(gulp.dest("src/css"));
 }
 
-//Minificar/concatenar/renomear arquivos CSS da pasta SRC para pasta DIST
-// function optimizeCSS() {
-//   return gulp
-//     .src(["src/css/**/*.css"])
-//     .pipe(cssmin())
-//     .pipe(concat("styles.css"))
-//     .pipe(rename({ suffix: ".min" }))
-//     .pipe(gulp.dest("dist/css"));
-// }
-
-// Minificar/concatenar/renomear arquivos JS Geral Dev
-// function optimizeJSGeneral() {
-//   return gulp
-//     .src(["src/js/**/*.js"])
-//     .pipe(uglify())
-//     .pipe(concat("scripts.js"))
-//     .pipe(rename({ suffix: ".min" }))
-//     .pipe(gulp.dest("dist/js"));
-// }
-
-// Otimizar arquivos de imagens Dev
-// function optimizeIMG() {
-//   return gulp
-//     .src("src/img/**/*")
-//     .pipe(imagemin())
-//     .pipe(gulp.dest("dist/img"));
-// }
-
-// Renomear links de CSS e JS minificados carregados no HTML Dev
-// function replaceHTML() {
-//   return gulp
-//     .src(["src/*.html"])
-//     .pipe(
-//       htmlreplace({
-//         allcss: "css/styles.min.css",
-//         alljs: "js/scripts.min.js",
-//       })
-//     )
-//     .pipe(gulp.dest("dist/"));
-// }
-
-
 // Agrupar tarefas a serem monitoradas
 function watch() {
   gulp.watch("src/scss/**/*.scss", compilaSASS);
-  // gulp.watch("src/css/**/*.css", optimizeCSS);
 }
 
 // Agrupar e executar tarefas
 const build = gulp.parallel(
   compilaSASS,
-  // optimizeCSS,
-  // optimizeJSGeneral,
-  // optimizeIMG,
-  // replaceHTML,
   watch
 );
 gulp.task("default", build);
